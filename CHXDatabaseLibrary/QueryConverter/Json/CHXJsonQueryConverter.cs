8e1389d0754a0e5eb2af2b7c545cd95e06177cae
Dinçer UYGUN <dncruygun@hotmail.com>
2017-03-16 08:21:24 +0300

    -

--- CHXDatabaseLibrary/QueryConverter/Json/CHXJsonQueryConverter.cs
+++ CHXDatabaseLibrary/QueryConverter/Json/CHXJsonQueryConverter.cs
@@ -17,20 +17,56 @@ namespace CHXDatabaseLibrary.QueryConverter.Json
             if (jsonObj.server == null) throw new Exception("Server adı bulunamadı");
             if (jsonObj.query == null) throw new Exception("Query bulunamadı");
 
+
+            var queryContainer = new QueryContainer();
+            queryContainer.Server = jsonObj.server;
+            queryContainer.Schema = jsonObj.schema;
+
+
+            queryContainer.Query = new QueryCollection();
+
             foreach (var q in jsonObj.query)
             {
-                var tableName = q.Name;
-                var queryType = q.Value.type.Value;
-                var addGeometry = q.Value.addgeometry.Value;
+                var _query = new QueryTable();
+
+                _query.TableName = q.Name;
+                _query.QueryType = q.Value.type.Value;
+                _query.AddGeometry = q.Value.addgeometry == null ? false : q.Value.addgeometry.Value;
 
                 foreach (var f in q.Value.field)
                 {
+                    string _tempVal = f.Value;
+
+                    if (_tempVal.IndexOf("=>") > -1) _query.Field.Add(new QueryField((_tempVal.Substring(0, _tempVal.IndexOf("=>") - 1).Trim()), _tempVal.Substring(_tempVal.IndexOf("=>") + 2, _tempVal.Length - _tempVal.IndexOf("=>") - 2).Trim()));
+                    else
+                        _query.Field.Add(new QueryField(_tempVal));
 
                 }
 
+                if (q.Value.find != null)
+                {
+                    foreach (var f in q.Value.find)
+                    {
+                        var findList = new QueryFindList();
+
+                        foreach (var item in f)
+                        {
+                            findList.Add(new QueryFind(item.Name, item.Value.Value));
+                        }
+
+                        _query.QueryFind.Add(findList);
+                    }
+                }
+
+
+                queryContainer.Query.Add(_query);
             }
 
-            return "";
+
+
+            
+
+            return queryContainer;
         }
     }
 }
--- CHXDatabaseLibrary/QueryConverter/QueryContainer.cs
+++ CHXDatabaseLibrary/QueryConverter/QueryContainer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,15 +17,198 @@ namespace CHXDatabaseLibrary.QueryConverter
     }
 
 
-    public class QueryTable : IQueryTable { }
-
-    public abstract class IQueryTable
+    [DebuggerDisplay("{ToString()}")]
+    public class QueryTable
     {
-        public string type { get; set; }
-        public bool addgeometry { get; set; }
-        public List<string> field { get; set; }
+        string _tableName;
+        string _queryType;
+        bool _addGeometry;
+        List<QueryField> _field;
+        List<QueryFindList> _queryFind;
+
+        public List<QueryField> Field
+        {
+            get
+            {
+                return _field;
+            }
+
+            set
+            {
+                _field = value;
+            }
+        }
+
+        public bool AddGeometry
+        {
+            get
+            {
+                return _addGeometry;
+            }
+
+            set
+            {
+                _addGeometry = value;
+            }
+        }
+
+        public string QueryType
+        {
+            get
+            {
+                return _queryType;
+            }
+
+            set
+            {
+                _queryType = value;
+            }
+        }
+
+        public string TableName
+        {
+            get
+            {
+                return _tableName;
+            }
+
+            set
+            {
+                _tableName = value;
+            }
+        }
+
+        public List<QueryFindList> QueryFind
+        {
+            get
+            {
+                return _queryFind;
+            }
+
+            set
+            {
+                _queryFind = value;
+            }
+        }
+
+        public QueryTable()
+        {
+            _field = new List<QueryField>();
+            _queryFind = new List<QueryFindList>();
+        }
+
+
+        public override string ToString()
+        {
+            return this.TableName;
+        }
     }
 
+
+
+    public class QueryFindList : List<QueryFind> { }
+
+    public class QueryFind
+    {
+        string _name;
+        object _value;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public object Value
+        {
+            get
+            {
+                return _value;
+            }
+
+            set
+            {
+                _value = value;
+            }
+        }
+
+
+        public QueryFind()
+        {
+
+        }
+
+        public QueryFind(string name, object value)
+        {
+            _name = name;
+            _value = value;
+        }
+    }
+
+
+
+    [DebuggerDisplay("{ToString()}")]
+    public class QueryField
+    {
+        string _name;
+        string _alias;
+
+        public string Alias
+        {
+            get
+            {
+                return _alias;
+            }
+
+            set
+            {
+                _alias = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public QueryField()
+        {
+
+        }
+
+        public QueryField(string name)
+        {
+            _name = name;
+        }
+
+
+        public QueryField(string name, string alias)
+        {
+            _name = name;
+            _alias = alias;
+        }
+
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
 
     public class QueryCollection : List<QueryTable>
     {
